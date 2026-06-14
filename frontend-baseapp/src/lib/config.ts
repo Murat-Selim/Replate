6def660388f4f5d448acd4e15cbb63d0dcd0f49e
Murat-Selim <muratselim1994@gmail.com>
2026-06-14 22:10:16 +0300

    updates baseapp configs

--- frontend-baseapp/src/lib/config.ts
+++ frontend-baseapp/src/lib/config.ts
@@ -1,6 +1,11 @@
 import { http, createConfig, createStorage, cookieStorage } from 'wagmi';
 import { baseAccount, injected } from 'wagmi/connectors';
 import { appChain } from "@/lib/network";
+import { Attribution } from 'ox/erc8021';
+
+const DATA_SUFFIX = Attribution.toDataSuffix({
+  codes: ['bc_7to91eav'],
+});
 
 export const config = createConfig({
   chains: [appChain],
@@ -15,4 +20,5 @@ export const config = createConfig({
   transports: {
     [appChain.id]: http(),
   },
+  dataSuffix: DATA_SUFFIX,
 });
